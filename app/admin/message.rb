bb6d15592e4b7d710cab535bc1bc4455e7660485
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-03-07 11:16:25 +0100

    admin

--- app/admin/message.rb
+++ app/admin/message.rb
@@ -1,17 +1,18 @@
 ActiveAdmin.register Message do
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  index do
+    selectable_column
+    column :id
+    column :recipient
+    column :body
+    column :sent_at
+    column :status do |message|
+      message.status.id.to_s + " - " + message.status.description
+    end
+    column :client
+    column :retrieved_at
+    column :created_at
+    default_actions
+  end
   
 end
