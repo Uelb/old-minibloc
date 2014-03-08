c7b0c824269012812672cbb9a10719038dfb59b1
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-03-08 09:32:11 +0100

    improve client admin panel

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Client do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :event_base_url, :name, :email
   #
   # or
   #
@@ -14,4 +14,23 @@ ActiveAdmin.register Client do
   #  permitted
   # end
   
+  index do 
+    selectable_column
+    column :id
+    column :email
+    column :name
+    column :api_key
+    column :event_base_url
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :email
+      f.input :name
+      f.input :event_base_url
+    end
+    f.actions
+  end
+
 end
